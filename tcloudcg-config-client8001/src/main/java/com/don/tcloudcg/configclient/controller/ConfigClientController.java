85e3eca157c43555d1850ecb3e8a07223a996742
tang05709 <tangyong0425@sina.com>
2020-05-04 23:09:32 +0800

    update test yml

--- tcloudcg-config-client8001/src/main/java/com/don/tcloudcg/configclient/controller/ConfigClientController.java
+++ tcloudcg-config-client8001/src/main/java/com/don/tcloudcg/configclient/controller/ConfigClientController.java
@@ -1,4 +1,22 @@
 package com.don.tcloudcg.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
 public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+
 }
